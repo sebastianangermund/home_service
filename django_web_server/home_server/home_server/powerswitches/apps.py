d1f0fe20efb2175a617ac1731c0052b19c0ffbf5
Sebastian Angermund <sebastianangermund@Sebastians-MacBook-Pro.local>
2020-02-26 20:56:26 +0100

    remove powerswitch receivers

--- django_web_server/home_server/home_server/powerswitches/apps.py
+++ django_web_server/home_server/home_server/powerswitches/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class PowerSwitchConfig(AppConfig):
     name = 'home_server.powerswitches'
     verbose_name = "PowerSwitch"
-
-    def ready(self):
-        import home_server.powerswitches.receivers
